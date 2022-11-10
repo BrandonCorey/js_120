037c0dba4cf25144c0ada0b66dd0c3f7e5fd5f0d
Brandon Corey <bcorey3660@gmail.com>
2022-11-10 01:16:59 -0500

    Adds first version of oo_rps

--- oo_rps.js
+++ oo_rps.js
@@ -8,27 +8,49 @@
 // Verbs: choose, compare
 const readline = require('readline-sync');
 
-function createPlayer(playerType) {
+function createPlayer() {
   return {
-    // player name
-    // player current move
-    playerType,
     move: null,
-
-    choose() {
-      if (this.isHuman()) {
-        
-      } else {
-        const choices = ['rock', 'paper', 'scissors'];
-        let randomIdx = Math.floor(Math.random() * choices.length);
-        this.move = choices[randomIdx];
-      }
-    },
-    isHuman() {
-      return this.playerType === 'human';
-    }
+    choices: ['rock', 'paper', 'scissors'],
   };
 }
+
+function createComputer() {
+  let playerObject = createPlayer();
+
+  let computerObject =  {
+
+    choose() {
+      let randomIdx = Math.floor(Math.random() * this.choices.length);
+      this.move = this.choices[randomIdx];
+      // These "this" uses work because the choose method isn't called here,
+      // it is called much later.
+      // At the point at which choose is called, the objects have been combined,
+      // and computerObject has all of playerObject's props
+    }
+  };
+  return Object.assign(playerObject, computerObject);
+}
+
+function createHuman() {
+  let playerObject = createPlayer();
+  let humanObject = {
+
+    choose() {
+      let choice;
+      while (true) {
+        console.log('Please choose rock, paper, or scissors:');
+        choice = readline.question();
+        if (this.choices.includes(choice)) break;
+        console.log('Sorry, plese enter a valid choice!')
+      }
+      this.move = choice;
+    },
+  };
+
+  return Object.assign(playerObject, humanObject);
+}
+
 
 function createMove() {
   return {
@@ -49,22 +71,56 @@ let compare = function(move1, move2) {
 };
 
 const RPSGame = {
-  human: createPlayer('human'),
-  computer: createPlayer('computer'), // Doing this because both are players that need to make choices
+  human: createHuman(),
+  computer: createComputer(), // Doing this because both are players that need to make choices
 
   displayWelcomeMessage() {
-    console.log('Welcome to Rock, Paper, Scissors');
+    console.clear();
+    console.log(
+      `Welcome to Rocks, Paper, Scissors!`
+    );
   },
 
   displayGoodbyeMessage() {
-    console.log('Thanks for playing Rock, Paper, Scissors, Goodbye!')
+    console.log('Thanks for playing Rock, Paper, Scissors, Goodbye!');
+  },
+
+  displayWinner() {
+    let humanMove = this.human.move;
+    let computerMove = this.computer.move;
+
+    console.log(`You chose: ${this.human.move}`);
+    console.log(`The computer chose: ${this.computer.move}`);
+
+    if ((humanMove === 'rock' && computerMove === 'scissors') ||
+        (humanMove === 'paper' && computerMove === 'rock') ||
+        (humanMove === 'scissors' && computerMove === 'paper')) {
+      console.log('You win!');
+    } else if ((humanMove === 'rock' && computerMove === 'paper') ||
+               (humanMove === 'paper' && computerMove === 'scissors') ||
+               (humanMove === 'scissors' && computerMove === 'rock')) {
+      console.log('Computer wins!');
+    } else {
+      console.log("It's a tie");
+    }
+  },
+
+  playAgain() {
+    console.log('Would you like to play again? (y/n)');
+    let answer = readline.question();
+    return answer.toLowerCase()[0] === 'y';
   },
 
   play() {
     this.displayWelcomeMessage(); // Everything that is an overall convern to the game, keep in the game engine (makes sense to)
-    this.human.choose();
-    this.computer.choose();
-    displayWinner();
+    while (true) {
+      this.human.choose();
+      this.computer.choose();
+      this.displayWinner();
+      if (!this.playAgain()) break;
+    }
     this.displayGoodbyeMessage();
   },
 };
+
+RPSGame.play();
