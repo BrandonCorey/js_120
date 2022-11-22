4a5fe11eb0f8aa55c52467155a51dd689b449eed
Brandon Corey <bcorey3660@gmail.com>
2022-11-21 21:21:47 -0500

    Updates examples for messing around

--- messingAround.js
+++ messingAround.js
@@ -1,16 +1,22 @@
 let obj1 = {
   banana: 'banana',
   addArr() {
-    return this.values.reduce((a, b) => a + b);
+    this.sum = this.values.reduce((a, b) => a + b);
   },
 };
 
 let obj2 = {
   values: [100, 145, 2345, 323, 234],
   sum: 0,
+};
 
+function add(num) {
+  this.sum += num;
 }
 
 
-obj2.sum = obj1.addArr.call(obj2, obj2.values);
-console.log(obj2.sum);
+
+obj1.addArr.call(obj2);
+console.log(obj2.sum); // 3147
+add.call(obj2, 5);
+console.log(obj2.sum)
