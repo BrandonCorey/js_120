07bfc1abdd51415443af8e01e61b28f2872093af
Brandon Corey <bcorey3660@gmail.com>
2022-12-08 00:53:27 -0500

    Adds board and square classes to ttt

--- lesson_5/oo_ttt.js
+++ lesson_5/oo_ttt.js
@@ -1,19 +1,42 @@
+class Square {
+  constructor(marker = " ") {
+    this.marker = marker;
+  }
+
+  static UNUSED_SQUARE = ' ';
+  static HUMAN_MARKER = 'x';
+  static COMPUTER_MARKER = 'O';
+
+  toString() {
+    return this.marker // JS calls Object.prototype.toString implicitly when trying to log an object (object Object). We override that implicit call with this
+  }
+}
+
 class Board {
   constructor() {
-    //STUB
-    // we need a way to model the 3x3 grid. Perhaps squares...
-    // What data structure should we use? Array, Object?
-    // What should the data structure store?
+    this.squares = {};
+
+    for (let cnt = 1; cnt < 10; cnt++) {
+      this.squares[cnt] = new Square();
+    }
+  }
+
+  display() {
+    console.log("");
+    console.log("     |     |");
+    console.log(`  ${this.squares["1"]}  |  ${this.squares["2"]}  |  ${this.squares["3"]}`);
+    console.log("     |     |");
+    console.log("-----+-----+-----");
+    console.log("     |     |");
+    console.log(`  ${this.squares["4"]}  |  ${this.squares["5"]}  |  ${this.squares["6"]}`);
+    console.log("     |     |");
+    console.log("-----+-----+-----");
+    console.log("     |     |");
+    console.log(`  ${this.squares["7"]}  |  ${this.squares["8"]}  |  ${this.squares["9"]}`);
+    console.log("     |     |");
+    console.log("");
   }
 }
-
-class Square {
-  constructor() {
-    //STUB
-    // Need a way to keep track of square's marker
-  }
-}
-
 
 class Row {
   constructor() {
@@ -59,3 +82,62 @@ class Computer extends Player {
     //STUB
   }
 }
+
+class TTTGame {
+  constructor() {
+    this.board = new Board();
+    // Need a board and two players
+  }
+
+  play() {
+    //SPIKE
+    this.displayWelcomeMessage();
+
+    while (true) {
+      this.board.display();
+
+      this.firstPlayerMoves();
+      if (this.gameOver()) break;
+
+      this.secondPlayerMoves();
+      if (this.gameOver()) break;
+
+      break; // <= Execute loop only once for now
+    }
+
+    this.displayResults();
+    this.displayGoodbyeMessage();
+  }
+
+  displayWelcomeMessage() {
+    console.log('Welcome to Tic Tac Toe!');
+  }
+
+  displayGoodbyeMessage() {
+    console.log('Thanks for playing Tic Tac Toe! Goodbye!');
+  }
+
+  displayResults() {
+    //STUB
+    // show the results of the game (win, lose, tie)
+  }
+
+  firstPlayerMoves() {
+    //STUB
+    // the first player makes a move
+  }
+
+  secondPlayerMoves() {
+    //STUB
+    // the second player makes a move
+  }
+
+  gameOver() {
+    //STUB
+    return false;
+  }
+}
+
+let game = new TTTGame();
+
+game.play();
