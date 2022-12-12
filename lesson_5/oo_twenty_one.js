148003b110abc167721fc7e7e1d597cf8f50df41
Brandon Corey <bcorey3660@gmail.com>
2022-12-12 01:16:42 -0500

    Adds dealing and shuffling to twenty one

--- lesson_5/oo_twenty_one.js
+++ lesson_5/oo_twenty_one.js
@@ -1,61 +1,71 @@
 class Card {
-  constructor() {
-    //STUB
-    // What sort of state does the card need?
-    // Rank? Suit? Points?
+  constructor(suit, value, weight) {
+    this.suit = suit;
+    this.value = value;
+    this.weight = weight;
+  }
+
+  static suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+  static values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+  getWeight() {
+    return this.weight;
   }
 }
 
 class Deck {
   constructor() {
-    //STUB
-    // What sort of state does the deck need?
-    // 52 cards
-    // Need some sort of data structure
-    // array, object, something else
+    this.cards = [];
+
+    Card.suits.forEach(suit => {
+      Card.values.forEach(value => {
+        let weight;
+  
+        if (/[JQK]/g.test(value)) weight = 10;
+        else if (value === 'A') weight = 11;
+        else weight = Number(value);
+  
+        let card = new Card(suit, value, weight);
+  
+        this.cards.push(card);
+      });
+    });
+
   }
 
   deal(){
     //STUB
     // does the dealer or deck deal
   }
+
+  getCards() {
+    return this.cards;
+  }
+
+  shuffle() {
+    for (let idx = this.cards.length - 1; idx > 0; idx--) {
+      let randIdx = Math.floor(Math.random() * (idx + 1));
+      [this.cards[idx], this.cards[randIdx]] = 
+      [this.cards[randIdx], this.cards[idx]];
+    }
+  }
+
+  count() {
+    return this.cards.length;
+  }
 }
 
 class Participant {
   constructor() {
-  //STUB
-  // what state do we need
-  // score, hand, amount of money?
-  // do we put all redundant behaviors from player and dealer here?
-  }
-}
-
-class Player extends Participant {
-  constructor() {
-  //STUB
-  // what state do we need
-  // score, hand, amount of money?
+    this.hand = [];
   }
 
-  hit() {
-    //STUB
-  }
-
-  stay() {
-    //STUB
-  }
-
-  score() {
-    //STUB
-  }
-}
-
-class Dealer extends Participant {
-  // Similar to player, do we need this?
-  constructor() {
-    //STUB
-    // what sort of state do we need?
-    // score, hand, amount of money?
+  static showHand(hand) {
+    let string = "";
+    hand.forEach(card => {
+      string += card.value + ' of ' + card.suit + ', ';
+    });
+    return string.slice(0, string.length - 2);
   }
 
   hit() {
@@ -74,44 +84,86 @@ class Dealer extends Participant {
     //STUB
   }
 
-  hide() {
+  getHand() {
+    return this.hand;
+  }
+}
+
+class Player extends Participant {
+  constructor() {
+    super();
+  //STUB
+  // what state do we need
+  // score, hand, amount of money?
+  }
+}
+
+class Dealer extends Participant {
+  // Similar to player, do we need this?
+  constructor() {
+    super();
     //STUB
+    // what sort of state do we need?
+    // score, hand, amount of money?
+  }
+
+  hide() {
+    return this.hand.slice(0, 1);
   }
 
   reveal() {
-    //STUB
-  }
-
-  deal() {
-    //STUB
-    // Will dealer deal, or will deck deal?
+    return this.hand
   }
 }
 
 class TwentyOneGame {
   constructor() {
-    //STUB
-    // What state does the game need?
-    // deck, two participants?
+    this.player = new Player();
+    this.dealer = new Dealer();
+    this.deck = new Deck();
+  }
+
+  startRound() {
+    this.dealCards();
+    this.showCards();
+    this.playerTurn();
+    this.dealerTurn();
   }
 
   start() {
     //SPIKE
     this.displayWelcomeMessage();
-    this.dealCards();
-    this.showCards();
-    this.playerTurn();
-    this.dealerTurn();
+
+    this.deck.shuffle();
+    this.startRound();
+
     this.displayResult();
     this.displayGoodbyeMessage();
   }
 
+  displayWelcomeMessage() {
+    this.prompt('Welcome to the game Twenty-One!');
+    console.log('');
+  }
+
   dealCards() {
-    //STUB
+    const playerHand = this.player.getHand();
+    const dealerHand = this.dealer.getHand();
+    const cards = this.deck.getCards();
+
+    for (let cnt = 0; cnt < 4; cnt++) {
+      if (cnt % 2 === 0) playerHand.push(cards.pop());
+      else dealerHand.push(cards.pop());
+    }
   }
 
   showCards() {
-    //STUB
+    const playerHand = this.player.getHand();
+    const dealerHand = this.dealer.hide();
+
+    this.prompt(`Your cards: ${Participant.showHand(playerHand)}`);
+    this.prompt(`Dealer's cards: ${Participant.showHand(dealerHand)}`);
+    console.log('');
   }
 
   playerTurn() {
@@ -122,11 +174,19 @@ class TwentyOneGame {
     //STUB
   }
 
-  displayResults() {
+  displayResult() {
     //STUB
   }
 
   displayGoodbyeMessage() {
-    //STUB
+    this.prompt('Thank you for playing the game Twenty One!');
+  }
+
+  prompt(string) {
+    console.log('=> ' + string);
   }
 }
+
+let game = new TwentyOneGame();
+
+game.start();
